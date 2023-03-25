4ff764812f73e9c49008fd6f3387e761d265e3b6
conrad-seo <124890171+conrad-seo@users.noreply.github.com>
2023-03-25 21:29:53 +0900

    User 수정 및 ErrorCode 수정

--- src/main/java/com/example/everyhanghae/exception/CustomErrorCode.java
+++ src/main/java/com/example/everyhanghae/exception/CustomErrorCode.java
@@ -15,13 +15,16 @@ public enum CustomErrorCode {
     DUPLICATE_NICKNAME(BAD_REQUEST, "중복된 닉네임이 존재합니다"),
     NOT_PROPER_PASSWORD(BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     NOT_PROPER_INPUTFORM(BAD_REQUEST, "입력한 형식이 맞지 않습니다."),
+    NOT_LENGTH(BAD_REQUEST, "입력한 문자열 길이가 맞지 않습니다."),
     NOT_PROPER_URLFORM(BAD_REQUEST, "입력한 URL 형식이 맞지 않습니다."),
     NOT_AUTHOR(BAD_REQUEST, "작성자만 삭제/수정할 수 있습니다."),
     WRONG_ADMIN_TOKEN(BAD_REQUEST, "관리자 암호가 틀려 등록이 불가능합니다."),
+    NOT_INPUT(BAD_REQUEST, "입력한 문자열 조건이 맞지 않습니다."),
+    NOT_EMAIL(BAD_REQUEST, "이메일에 @가 없습니다"),
 
     /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
     USER_NOT_FOUND(NOT_FOUND, "등록된 사용자가 없습니다"),
-    POST_NOT_FOUND(NOT_FOUND, "선택한 게시물을 찾을 수 없습니다."),
+    BOARD_NOT_FOUND(NOT_FOUND, "선택한 게시물을 찾을 수 없습니다."),
     COMMENT_NOT_FOUND(NOT_FOUND, "선택한 댓글을 찾을 수 없습니다."),
     SECRET_KEY_NOT_FOUND(NOT_FOUND, "해당 시크릿 키는 존재하지 않습니다.")
 
@@ -29,6 +32,6 @@ public enum CustomErrorCode {
 
     private final HttpStatus httpStatus;
     private final String message;
-    
+
 }
 
