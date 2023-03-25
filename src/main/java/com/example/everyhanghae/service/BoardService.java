1e09a94dda3373b208d6cfd31622c128032e43db
이사라 <zbd0329@gmail.com>
2023-03-25 17:36:46 +0900

    feat: board delete, update 구현

--- src/main/java/com/example/everyhanghae/service/BoardService.java
+++ src/main/java/com/example/everyhanghae/service/BoardService.java
@@ -5,6 +5,7 @@ import com.example.everyhanghae.dto.response.BoardTypeResponseDto;
 import com.example.everyhanghae.entity.Board;
 import com.example.everyhanghae.entity.BoardType;
 import com.example.everyhanghae.entity.User;
+import com.example.everyhanghae.exception.CustomException;
 import com.example.everyhanghae.repository.BoardRepository;
 import com.example.everyhanghae.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.everyhanghae.exception.CustomErrorCode.NOT_AUTHOR;
+import static com.example.everyhanghae.exception.CustomErrorCode.POST_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -48,5 +52,43 @@ public class BoardService {
     @Transactional
     public void createPost(BoardRequestDto postRequestDto, User user) {
         boardRepository.saveAndFlush(new Board(postRequestDto, user));
+    }
+    @javax.transaction.Transactional
+    public void updatePost(Long boardId, BoardRequestDto boardRequestDto, User user) {
+        Board board = isExistBoard(boardId);
+        isAuthor(board,user);
+
+//        게시글 작성자 일치여부 확인 > 공통메서드 처리
+//        if (!board.getUser().getId().equals(user.getId())) {
+//            throw new CustomException(NOT_AUTHOR);
+//        }
+
+        // 게시글 수정
+        board.update(boardRequestDto.getTitle(), boardRequestDto.getContent());
+        boardRepository.save(board);
+
+
+    }
+
+    public void deletePost(Long boardId, User user) {
+        Board board = isExistBoard(boardId);
+        isAuthor(board,user);
+        boardRepository.deleteById(boardId);
+
+    }
+
+
+    //게시글 존재 하는지 확인 하는 공통 메서드
+    public Board isExistBoard(Long id){
+        return boardRepository.findById(id).orElseThrow(
+                () -> new CustomException(POST_NOT_FOUND)
+        );
+    }
+
+    //작성자가 일치 하는지 확인 하는 공통 메서드
+    public void isAuthor(Board board, User user) {
+        if (!board.getUser().getId().equals(user.getId())) {
+            throw new CustomException(NOT_AUTHOR);
+        }
     }
 }
